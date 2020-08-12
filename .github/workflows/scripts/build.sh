5ad6a942c796102b2a1aa8cdf7b05907e7a52da0
atlus117 <atlus117@gmail.com>
2020-08-11 23:03:54 -0500

    Update build.sh

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,10 +1,13 @@
-filename=$(basename)
-filename=${filename%.*}
-buildPath="build/$filename"
-dotnet publish --configuration Release $ --output $buildPath
-mv $buildPath/wwwroot/ "build/$filename"
-rm -rf $buildPath
+echo projects=($(ls -1 -- ../../../**/*.csproj))
+for i in "${projects[@]}"
+do
+    filename=$(basename -- "$i")    
+    filename="${filename%.*}"
+    buildPath="build/$filename-t"    
+	dotnet publish --configuration Release $i --output $buildPath
+    mv $buildPath/wwwroot/ "build/$filename"        
+    rm -rf $buildPath
 
-
-sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/Squibi\/$filename\/\" \/>/g" build/$filename/index.html
+    sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/BlazorCanvas\/$filename\/\" \/>/g" build/$filename/index.html
+done
 cp readme.md build/readme.md
