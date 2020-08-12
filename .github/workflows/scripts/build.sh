ed4b6b1a4091527155ac0b94c028e6bebd66570b
atlus117 <atlus117@gmail.com>
2020-08-12 01:18:05 -0500

    renamed fold to match namespace

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -2,8 +2,11 @@ projects=($(ls -1 -- **/*.csproj))
 for i in "${projects[@]}"
 do
     filename=$(basename -- "$i")    
+    echo $filename
     filename="${filename%.*}"
+    echo $filename
     buildPath="build/$filename-t"    
+    echo $buildPath
 	dotnet publish --configuration Release $i --output $buildPath
     mv $buildPath/wwwroot/ "build/$filename"        
     rm -rf $buildPath
